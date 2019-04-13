187ed78141cd33f9ef8689f004fb2080d603222d
Christopher Lee <sehlceris@protonmail.com>
2019-04-13 12:51:12 -0700

    do not modify records if they already point to current external IP

--- index.js
+++ index.js
@@ -35,8 +35,18 @@ async function main() {
   const zone = await dns.zone(zoneName);
   const records = (await zone.getRecords())[0];
 
-  // find existing records that we need to modify/remove
-  const recordsToModify = records.filter((record) => dnsNamesSet.has(record.name) && record.type === 'A');
+  // find existing records that match the DNS names we want to set
+  const recordsOfInterest = records.filter((record) => dnsNamesSet.has(record.name) && record.type === 'A');
+
+  // filter out records that already match our external IP (no need to modify them)
+  const recordsToModify = recordsOfInterest.filter((record) => {
+    if (record.data && record.data[0] && record.data[0] === externalIp) {
+      return false;
+    }
+    return true;
+  });
+
+  // modifying a record entails removing and then re-adding the record
   const recordsToRemove = [...recordsToModify];
   const modifiedRecords = recordsToModify.map((record) => {
     return zone.record('a', {
@@ -47,7 +57,7 @@ async function main() {
   });
 
   // dns names are in the config that did not previously exist, create those records
-  const dnsNamesThatNeedToBeAdded = dnsNames.filter((dnsName) => !recordsToModify.find((record) => record.name === dnsName));
+  const dnsNamesThatNeedToBeAdded = dnsNames.filter((dnsName) => !recordsOfInterest.find((record) => record.name === dnsName));
   const newRecords = dnsNamesThatNeedToBeAdded.map((dnsName) => {
     return zone.record('a', {
       name: dnsName,
@@ -62,20 +72,25 @@ async function main() {
     ...modifiedRecords,
   ];
 
-  recordsToRemove.forEach((record) => {
-    console.log(`removing record: ${record.name} | ${record.data} | ${record.metadata.ttl}`);
-  });
-  recordsToAdd.forEach((record) => {
-    console.log(`adding record: ${record.name} | ${record.data} | ${record.metadata.ttl}`);
-  });
+  if (!recordsToAdd.length && !recordsToRemove.length) {
+    console.log('no records to modify');
+  }
+  else {
+    recordsToRemove.forEach((record) => {
+      console.log(`removing record: ${record.name} | ${record.data} | ${record.metadata.ttl}`);
+    });
+    recordsToAdd.forEach((record) => {
+      console.log(`adding record: ${record.name} | ${record.data} | ${record.metadata.ttl}`);
+    });
 
-  // effect the change
-  const changeConfig = {
-    delete: recordsToRemove,
-    add: recordsToAdd,
-  };
-  const change = await zone.createChange(changeConfig);
-  console.log('change completed');
+    // effect the change
+    const changeConfig = {
+      delete: recordsToRemove,
+      add: recordsToAdd,
+    };
+    const change = await zone.createChange(changeConfig);
+    console.log('change completed');
+  }
 }
 
 main()
